b834a30189ae705e6e3864690c34cbdc7d76aa68
ft529y <yuto529reds@gmail.com>
2021-10-22 21:40:19 +0900

    weightsマイグレーションファイル記述

--- db/migrate/20211022123021_create_weights.rb
+++ db/migrate/20211022123021_create_weights.rb
@@ -1,7 +1,9 @@
 class CreateWeights < ActiveRecord::Migration[6.0]
   def change
     create_table :weights do |t|
-
+      t.string :weight,           null: false
+      t.string :height
+      t.references :user,         null: false, foreign_keys: true
       t.timestamps
     end
   end
