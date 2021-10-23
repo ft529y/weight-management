e14715ddee99909ece79a1d7d05582ab24b1d737
ft529y <yuto529reds@gmail.com>
2021-10-24 05:01:18 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'weights#index'
   resources :weights, only: [:new, :create]
-  resources :user, only: [:show]
+  resources :users, only: [:show]
 end
