593715289fb52334f2ac01d8c03403903801220c
ft529y <yuto529reds@gmail.com>
2021-11-06 17:27:03 +0900

    mealマイグレーションファイル記述

--- db/migrate/20211106082132_create_meals.rb
+++ db/migrate/20211106082132_create_meals.rb
@@ -1,7 +1,8 @@
 class CreateMeals < ActiveRecord::Migration[6.0]
   def change
     create_table :meals do |t|
-
+      t.string :eat_meal
+      t.references :user,    null: false, foreign_keys: true
       t.timestamps
     end
   end
