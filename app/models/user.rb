f6815e6532afb6a623d6dc974fc2cef050e14f13
ft529y <yuto529reds@gmail.com>
2021-11-07 20:13:00 +0900

    user.rb rubocop実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :weights
   has_many :meals
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -20,6 +20,4 @@ class User < ApplicationRecord
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
     validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'パスワードは英数字を含めて下さい。' }
   end
-
-  
 end
