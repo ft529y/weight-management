9b16d703751a88a50f61983e0ff06ab08f22f4a1
ft529y <yuto529reds@gmail.com>
2021-11-07 18:54:14 +0900

    user.rb バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :birthday
+    validates :initial_weight
+    validates :height
+    VALID_NAME_REGIX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+    with_options format: { with: VALID_NAME_REGIX, message: 'は全角文字を入力して下さい' } do
+      validates :last_name
+      validates :first_name
+    end
+
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'パスワードは英数字を含めて下さい。' }
+  end
+
+  
 end
